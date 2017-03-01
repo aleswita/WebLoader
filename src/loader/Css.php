309b43e91c3f3f6163854ec53f9769933bc49ddd
Aleš <aleswita+githubcom@gmail.com>
2017-03-01 12:49:45 +0100

    Create Css.php

---
+++ src/loader/Css.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * This file is part of the AlesWita\Components\WebLoader
+ * Copyright (c) 2017 Ales Wita (aleswita+github@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace AlesWita\Components\WebLoader\Loader;
+
+use AlesWita;
+use AlesWita\Components\WebLoader\Factory;
+use Nette\Caching;
+use Nette\Utils;
+
+
+/**
+ * @author Ales Wita
+ * @license MIT
+ */
+class Css extends Loader
+{
+	/**
+	 * @param array
+	 * @return AlesWita\Components\WebLoader\Loader\ILoader
+	 */
+	public function setFiles(array $files): AlesWita\Components\WebLoader\Loader\ILoader {
+		if (isset($files[Factory::FILE_TAG_CSS])) {
+			$this->files = $files[Factory::FILE_TAG_CSS];
+		} else {
+			$this->files = [];
+		}
+		return $this;
+	}
+
+	/**
+	 * @return void
+	 */
+	public function render(): void {
+		echo $this->cache->load("namespace-{$this->namespace}-tag-" . Factory::FILE_TAG_CSS, function (& $dp): string {
+			$dp = [Caching\Cache::TAGS => [Factory::CACHE_TAG]];
+
+			$main = Utils\Html::el();
+
+			foreach ($this->files as $file) {
+				$html = Utils\Html::el("link")
+					->setRel("stylesheet")
+					->setHref($file);
+
+				$main->insert(NULL, $html);
+			}
+
+			return $main->render(1);
+		});
+	}
+}
