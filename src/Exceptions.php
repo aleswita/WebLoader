d826e3380b6c6b1c7ca653fbc66cd50bb08119eb
Aleš <aleswita+githubcom@gmail.com>
2017-03-01 12:48:32 +0100

    Create Exceptions.php

---
+++ src/Exceptions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the AlesWita\Components\WebLoader
+ * Copyright (c) 2017 Ales Wita (aleswita+github@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace AlesWita\Components\WebLoader;
+
+
+class WebLoaderException extends \Exception
+{
+}
