088c4fb5f03508877f2c0c2d2cc0b91fdef11d35
Aleš <aleswita+githubcom@gmail.com>
2017-03-01 12:50:18 +0100

    Create ILoader.php

---
+++ src/loader/ILoader.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file is part of the AlesWita\Components\WebLoader
+ * Copyright (c) 2017 Ales Wita (aleswita+github@gmail.com)
+ */
+
+declare(strict_types=1);
+
+namespace AlesWita\Components\WebLoader\Loader;
+
+use AlesWita;
+use Nette;
+use Nette\Application;
+
+
+/**
+ * @author Aleš Wita
+ * @license MIT
+ */
+interface ILoader
+{
+	/**
+	 * @param array
+	 * @return AlesWita\Components\WebLoader\Loader\ILoader
+	 */
+	function setFiles(array $files): AlesWita\Components\WebLoader\Loader\ILoader;
+
+	/**
+	 * @param string
+	 * @return AlesWita\Components\WebLoader\Loader\ILoader
+	 */
+	function setNamespace(string $namespace): AlesWita\Components\WebLoader\Loader\ILoader;
+
+	/**
+	 * @param Nette\Caching\Cache
+	 * @return AlesWita\Components\WebLoader\Loader\ILoader
+	 */
+	public function setCache(Nette\Caching\Cache $cache): AlesWita\Components\WebLoader\Loader\ILoader;
+
+	/**
+	 * @return void
+	 */
+	function render(): void;
+}
